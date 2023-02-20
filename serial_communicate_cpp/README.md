564c19159ed6671600959aa3eeee29241eb93fb6
Dmitry <56763340+dmitrypavlikov@users.noreply.github.com>
2023-02-20 21:03:41 +0300

    Initial commit

---
+++ serial_communicate_cpp/README.md
@@ -0,0 +1,3 @@
+# ALEX_serial_cpp
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
---
+++ serial_communicate_cpp/src/mail_node.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world serial_communicate_cpp package\n");
+  return 0;
+}
