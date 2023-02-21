4c7bae9c8a872a14ca23f775f9595f27944086ec
Dmitry <56763340+dmitrypavlikov@users.noreply.github.com>
2023-02-21 20:49:30 +0300

    Continue

--- serial_communicate_cpp/src/mail_node.cpp
+++ serial_communicate_cpp/src/mail_node.cpp
@@ -1,10 +1,54 @@
+#include <chrono>
 #include <cstdio>
+#include <cstdlib>
 
-int main(int argc, char ** argv)
+#include "rclcpp/rclcpp.hpp"
+#include "custom_msgs/msg/custom.hpp"
+
+using namespace std::chrono_literals;
+
+/*class MailPublisher : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+  public:
+    Publisher()
+    : Node("mail_publisher"), count_(0)
+    {
+      publisher_ = this->create_publisher<custom_msgs::msg::String>("topic", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+	private:
+    void topic_callback(const std_msgs::msg::String & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};*/
 
-  printf("hello world serial_communicate_cpp package\n");
+//Waiting for custom msg
+
+
+void recieveMail(){
+	//Receive UART mail (+XXXX,+XXXX,XXX,XXX.)
+	// ...
+	//Convert mail to int variables
+	// ...
+	;
+}
+
+void sendMail(){
+	//Create mail package (+XXXX,+XXXX,XXX,XXX.)
+	// ...
+	//Send UART mail to DXL_controller
+	// ...
+	;
+}
+
+int main()
+{
+
+
+  printf("YASS");
   return 0;
 }
+
